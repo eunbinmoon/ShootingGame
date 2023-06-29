02d6db774b93d88ef0f408454d2dffb6fff886e0
eunbinmoon <bin1109a@gmail.com>
2023-06-29 15:58:31 +0900

    초기셋팅 중

---
+++ main.js
@@ -0,0 +1,8 @@
+//캔버스 세팅
+let canvas;
+let ctx;
+canvas = document.createElement("canvas");
+ctx = canvas.getContext("2d");
+canvas.width = 400;
+canvas.height = 700;
+document.body.appendChild(canvas);
